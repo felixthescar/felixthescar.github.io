3a8b753b42faf6fbf68a4ea4727e38cac672037b
felixthescar <felixscarmoc2@protonmail.com>
2021-06-06 02:50:38 +0300

    commit

--- script.js
+++ script.js
@@ -10,10 +10,4 @@ function alertBox() {
     alert("Ati fost adaugat in baza de date.");
 }
 
-window.onload = displayClock();
-function displayClock() {
-	let time = new Date().toLocaleTimeString();
-
-	document.getElementById("clock").innerText = time;
-	setTimeout(displayClock, 1000); /* setting timer */
-}
+/* */
