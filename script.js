fa1ca726df78c905f9fedb24abcf419cfd07f7da
Nicu Theodor Alexandru <nicu.andru@yahoo.com>
2021-10-12 19:47:32 +0300

    Unit fork-ul meu cu main repo

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("openBtn").style.visibility = "hidden";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("openBtn").style.visibility = "visible";
 }
